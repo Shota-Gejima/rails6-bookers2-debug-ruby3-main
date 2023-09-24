46c9d25e8f628f0f085d499e712436b365ba4243
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-09-24 10:31:41 +0000

    [finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,11 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    
+    @book_new = Book.new
+    
+
+    
   end
 
   def index
@@ -16,32 +21,47 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
+      
     end
   end
 
   def edit
+    
+    book = Book.find(params[:id])
+    unless book.user == current_user
+      redirect_to '/books'
+    end
+    
+    
     @book = Book.find(params[:id])
-  end
+    
+    end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book), notice: "You have Updated book successfully."
     else
       render "edit"
     end
   end
-
+    
   def destroy
+    
     book = Book.find(params[:id])
+    
     book.destroy
-    redirect_to books_path
+
+    redirect_to '/books'
+    
   end
 
   private
 
   def book_params
+
     params.require(:book).permit(:title, :body)
   end
+  
 end
