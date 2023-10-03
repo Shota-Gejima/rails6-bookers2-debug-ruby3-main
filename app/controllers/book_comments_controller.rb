4793ccda0818a7fc630c80bcae53fb98b935a16a
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-10-03 04:53:57 +0000

    [comment-Ajax-finish]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -12,7 +12,7 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
+    @book = Book.find(params[:book_id])
     @book_comment = BookComment.find(params[:id])
     @book_comment.destroy
     @book_comment = BookComment.new
